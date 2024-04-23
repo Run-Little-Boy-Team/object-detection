f3921d5d23e70acb5fc90c68dbbbafe5b35d3194
Klbgr <qiuantoine@gmail.com>
2024-04-23 19:40:55 +0200

    added logging to file

--- utils.py
+++ utils.py
@@ -1,11 +1,31 @@
-from logging import getLogger, basicConfig, Logger
+from logging import (
+    getLogger,
+    Logger,
+    Formatter,
+    FileHandler,
+    StreamHandler,
+)
+from datetime import datetime
+from os import makedirs
 
 
-def init_logger(name: str = "System", log_level: str = "INFO") -> Logger:
+def init_logger(
+    name: str = "System", log_level: str = "INFO", save: bool = True
+) -> Logger:
     logger = getLogger(name)
-    basicConfig(
-        format="[%(asctime)s %(levelname)s - %(name)s] %(message)s",
-        datefmt="%H:%M:%S",
-        level=log_level,
-    )
+    logger.setLevel(log_level.upper())
+    formatter = Formatter("[%(asctime)s %(levelname)s - %(name)s] %(message)s")
+    formatter.datefmt = "%H:%M:%S"
+
+    console_handler = StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    if save:
+        makedirs("./logs", exist_ok=True)
+        file_handler = FileHandler(
+            datetime.now().strftime(f"./logs/{name}_%Y%m%d%H%M%S.log")
+        )
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
     return logger
